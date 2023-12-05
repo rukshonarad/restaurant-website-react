707d16b49950d3053c6bae140e6eb17de2cedd6c
Rukshona Rad <rukshona.rad@gmail.com>
2023-12-04 23:08:26 -0500

    Refactor Chefs Section to props

--- src/pages/home/sections/chefs/Chefs.js
+++ src/pages/home/sections/chefs/Chefs.js
@@ -1,7 +1,18 @@
-import betran from "../../../../assets/betran.png";
-import ferry from "../../../../assets/ferry.png";
-import iswan from "../../../../assets/iswan.png";
+import chef1 from "../../../../assets/betran.png";
+import chef2 from "../../../../assets/ferry.png";
+import chef3 from "../../../../assets/iswan.png";
 import "./Chefs.css";
+
+const Chef = (props) => {
+    return (
+        <div class="chefs-section__chef">
+            <img src={props.img} alt="Betran Chef" />
+            <h4>{props.name}</h4>
+            <p>{props.title}</p>
+        </div>
+    );
+};
+
 const Chefs = () => {
     return (
         <section id="chefs-section">
@@ -10,21 +21,9 @@ const Chefs = () => {
                     Our Greatest famous <span>Chefs</span>
                 </h2>
                 <div class="chefs-section__chefs">
-                    <div class="chefs-section__chef">
-                        <img src={betran} alt="Betran Chef" />
-                        <h4>Betran Komar</h4>
-                        <p>Lead chef</p>
-                    </div>
-                    <div class="chefs-section__chef">
-                        <img src={ferry} alt="Ferry chef" />
-                        <h4>Ferry Sauwi</h4>
-                        <p>Chef</p>
-                    </div>
-                    <div class="chefs-section__chef">
-                        <img src={iswan} alt="Iswan Chef" />
-                        <h4>Iswan Dracho</h4>
-                        <p>Chef</p>
-                    </div>
+                    <Chef img={chef1} name="Betran Komar" title="Head Chef" />
+                    <Chef img={chef2} name="Iswan Dracho" title=" Chef" />
+                    <Chef img={chef3} name="Ferri Sauwi" title="Chef" />
                 </div>
                 <button class="btn btn-lg btn-orange">View All</button>
             </div>
